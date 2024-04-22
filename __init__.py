0cb3a8fb9f5474d92afc5336f5740be1e1307074
Craig Jones <artistcdmj@gmail.com>
2024-04-22 12:14:13 -0500

    Update __init__.py
    trying to correct the register class

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Stencil Widget",
     "description": "Show a widget to control the stencil brush",
     "author": "kgeogeo, Spirou4D",
-    "version": (1, 2),
+    "version": (1, 3),
     "blender": (2, 7, 8),
     "location": "ctrl + shift +Q > Texture Stencil  OR shift + alt + Q > Mask Stencil",
     "warning": "",
@@ -14,7 +14,9 @@ import bpy
 from . import stencil_widget, mask_stencil_widget
 
 def register():
-    bpy.utils.register_module(__name__)
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
     km_list = ['Image Paint', 'Sculpt', 'Vertex Paint']
     for i in km_list:
         km = bpy.context.window_manager.keyconfigs.default.keymaps[i]
@@ -29,7 +31,9 @@ def unregister():
             km.keymap_items.remove(kmi)
         for kmi in (kmi for kmi in km.keymap_items if kmi.idname in {"brush.stencil_widget", }):
             km.keymap_items.remove(kmi)
-    bpy.utils.unregister_module(__name__)
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
 
 if __name__ == "__main__":
     register()
